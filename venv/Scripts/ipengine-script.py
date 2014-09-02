5b74065ee9feaa44dea9de70ad7cf3bf1e8c6566
Dave Walker <oddsockmachine@gmail.com>
2014-09-02 23:20:06 +0100

    Added virtual env because firewall is blocking pip

---
+++ venv/Scripts/ipengine-script.py
@@ -0,0 +1,9 @@
+#!C:\B_Py\distest\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'ipython==1.1.0','console_scripts','ipengine'
+__requires__ = 'ipython==1.1.0'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('ipython==1.1.0', 'console_scripts', 'ipengine')()
+)
